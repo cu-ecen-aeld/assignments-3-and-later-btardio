89fc0b862b9201f41ab45effe0cd755e8caf4174
EC2 Default User <ec2-user@ip-10-0-207-80.us-west-1.compute.internal>
2025-02-02 05:36:03 +0000

    updates

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -48,7 +48,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    make -j8 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
 
 
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
